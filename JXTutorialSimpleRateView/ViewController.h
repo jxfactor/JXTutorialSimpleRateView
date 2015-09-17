e1d3b01d8895586eaba4877fa026dd7b661cfb68
jxfactor <kingjun15@126.com>
2015-09-17 13:34:18 +0800

    Initial Commit

---
+++ JXTutorialSimpleRateView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXTutorialSimpleRateView
+//
+//  Created by 汪骏祥 on 9/17/15.
+//  Copyright (c) 2015 junxiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JXTutorialSimpleRateView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JXTutorialSimpleRateView
+//
+//  Created by 汪骏祥 on 9/17/15.
+//  Copyright (c) 2015 junxiang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
